4c53241c245445f7b1c07795d4b3b4d92026aadc
victor-dv <js813417@gmail.com>
2023-07-26 21:53:31 -0300

    Criar html, css e script

---
+++ script.js
@@ -0,0 +1,12 @@
+/* 
+indrotução jaascript */
+
+
+var nome = "Samuel"
+let idade = 45
+const anoNascimento = 1977;
+let idadeEsposa = "45"
+
+
+console.log(idade === idadeEsposa);
+ let listaCompras = ["Maçã" , "Manga" , "Maracujá", 13, ]
