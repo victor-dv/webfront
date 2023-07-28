8b9987da18b707611c6600554977ec2ce125ecbe
victor-dv <js813417@gmail.com>
2023-07-27 21:34:54 -0300

    Atividade

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-/* 
-indrotução jaascript */
+const titulo = document.getElementById("titulo")
+//input
+const nome = document.getElementById("nome")
+const email = document.getElementById("email")
+const telefone = document.getElementById("telefone")
+
+//output
+const resultadoNome = document.getElementById("resultadoNome")
+const resultadoEmail = document.getElementById("resultadoEmail")
+const resultadoTelefone = document.getElementById("resultadoTelefone")
 
 
-var nome = "Samuel"
-let idade = 45
-const anoNascimento = 1977;
-let idadeEsposa = "45"
 
 
-console.log(idade === idadeEsposa);
- let listaCompras = ["Maçã" , "Manga" , "Maracujá", 13, ]
+function salvar(){
+ resultadoNome.textContent = nome.value 
+ resultadoEmail.textContent = email.value
+resultadoTelefone.textContent = telefone.value
 
- // se o valor é verdadeiro nao precisa colocar no if === true
- /* var isOld = true;
- if(isOld){
-
- }
- */
-
- if (idade === idadeEsposa) {
-    console.log ("Igual")
- }
- else if(idade =! idadeEsposa){
-    console.log("diferente")
- }else{
-    
- }
-
- /* Array
-  accsesando oo array fazendo referencia ao index
- */
-
-  let lista = [1,2,3,4,5,6]
-
-  for (let index = 0; index < lista.length; index++) {
-    const element = lista[index];   
-    
-    console.log(element)
-  } 
+}
