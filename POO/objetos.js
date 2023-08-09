dde68d4414876e22701b4172fd01e54b61943faf
victor-dv <js813417@gmail.com>
2023-08-08 21:53:14 -0300

    Atividade day

--- POO/objetos.js
+++ POO/objetos.js
@@ -2,12 +2,14 @@
 //1. Forma Literal
 const userName = document.getElementById("name") 
 const prod = document.getElementById("product")
-const cor = document.getElementById("color")
+const color = document.getElementById("cor")
 const button = document.getElementById("btn")
 
 const person = {
     name: "Samuel Santos",
-    age: 45,
+    born: 2005,
+    mes: 12,
+    day: 11,
     weight: "98kb",
     gender: "Mininu",
     height: 1.83,
@@ -23,21 +25,59 @@ const person = {
         ocuppation: "Instutot PIII",
         salary: "116.235,89"
     },
-} //atributos
+
+    /// Criando metodos
+calcAge: function(){
+    var d = new Date;
+    anoAtual = d.getFullYear(),
+    mesAtual = d.getMonth() + 1,
+    diaAtual = d.getDate(),
+
+    this.born = +this.born,
+    this.mes = +this.mes,
+    this.day = +this.day,
+
+    currentData = anoAtual - this.born;
+
+    if(mesAtual < this.mes || mesAtual == this.mes && diaAtual < this.day ){
+        return currentData 
+        
+
+    }
+
+
+
+
+
+  /*   const currentData = this.hoje.getFullYear() - this.born.getFullYear()
+    if(new Date(this.hoje.getFullYear(), this.hoje.getMonth(), this.hoje.getDate()) <
+    new Date (this.hoje.getFullYear(), this.mes.getMonth(), this.born.getDate() ) ) 
+    return currentData.getFullYear() - this.born;
+    return currentData */
+
+}
+}
+
+ //atributos
 
 let produto = {}
 let produtos = []
 
-button.addEventListener('clic', ()=>{
+button.addEventListener('click', ()=>{
+
     produto = {
+        // Gera id randomico arredondando
         id: Math.floor(Math.random() * 21),
         name:prod.value,
-        cor: cor.value
+        color: color.value
     }
     //Metodos para adicionar Lista
     produtos.push(produto)
-    console.log(produto)
+    console.log(produtos)
+    
+    console.log(person.calcAge())
 })
+
 
 userName.textContent = person.name
 
@@ -45,3 +85,7 @@ userName.textContent = person.name
 // formas de accessar
 //1. person.(nome atrinbuto)
 //2.person["nome atributo"]
+
+
+// pedir dado novo
+// if ajustar 
