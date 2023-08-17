8c0956ec872f936926b7f2ff2e893f3be804debb
victor-dv <js813417@gmail.com>
2023-08-16 21:53:00 -0300

    Atividade Feita

--- atividadeMudaCor/index.js
+++ atividadeMudaCor/index.js
@@ -1,11 +1,35 @@
 //querySelectorall devolve uma lista 
-
-const colorsA = document.querySelectorAll('.colorA')
-
-colorsA.forEach(colorA =>{
+const minhaDiv = document.getElementById("minha-div")
+const elementos = document.getElementById("elementos")
+//Criar a lista com as cores
+const listColors = [
+    { cor: 'blue', nome: 'Azul' }, 
+    { cor: 'yellow', nome: 'Amarelo' },
+    { cor: 'green', nome: 'Verde' },
+    { cor: 'grey', nome: 'Cinza' },
+    { cor: 'gold', nome: 'Dourado' },
+    { cor: 'white', nome: 'Branco' },
+]
+//Criar elementos a partir da lista
+listColors.forEach(elemento => {
+    //Criei o botão e em seguida, atribuir os valores
+    let buttonColor = document.createElement('button')
+    //Propriedades do botão
+    buttonColor.id = elemento.cor
+    buttonColor.textContent = elemento.nome
+    buttonColor.style.borderColor = elemento.cor
+    buttonColor.style.color = 'black'   
+    //Adcionei o listener...
+    buttonColor.addEventListener('click', () => {
+        minhaDiv.style.backgroundColor = elemento.cor
+    })
+    elementos.appendChild(buttonColor)
+})
+/* colorsA.forEach(element =>{
     colorA.addEventListener('click',()=>{
-     minhaDiv.style.backgroundColor = elemnt.id
+     minhaDiv.style.backgroundColor = element.id
         
     })
 
-})
+}) */
+
